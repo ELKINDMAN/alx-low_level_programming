93d8b9740c3fb1c32fc56946ea6f1c93f892738a
ELKINDMAN <elkanahkindness@gmail.com>
2023-06-19 19:15:50 -0700

    All possible different combinations of three digits in task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible different combinations of three digits.
+ * Numbers must be separated by a comma and a space.
+ * The three digits must be different.
+ * 012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2.
+ * Print only the smallest combination of three digits.
+ * Numbers should be printed in ascending order, with three digits.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hundreds_digit;
+	int tens_digit;
+	int ones_digit;
+
+	for (hundreds_digit = 0; hundreds_digit <= 7; hundreds_digit++)
+	{
+		for (tens_digit = hundreds_digit + 1; tens_digit <= 8; tens_digit++)
+		{
+			for (ones_digit = tens_digit + 1; ones_digit <= 9; ones_digit++)
+			{
+				putchar(hundreds_digit + '0');
+				putchar(tens_digit + '0');
+				putchar(ones_digit + '0');
+
+				if (hundreds_digit != 7 || tens_digit != 8 || ones_digit != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
