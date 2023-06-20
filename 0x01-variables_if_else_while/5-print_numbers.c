06366d0d888f0fe9ce268ff23720eaa6dfe02492
ELKINDMAN <elkanahkindness@gmail.com>
2023-06-19 18:19:57 -0700

    Add code for printing numbers 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the numbers 0 to 10
+ *              using putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (; num <= 10; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
