01de820c9459d8c5eb503024aef451a9ab10e984
ELKINDMAN <elkanahkindness@gmail.com>
2023-08-12 14:36:19 -0700

    task complete

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+* main - Entry point
+*
+* Description: A program that prints using puts
+* Author: elkanah kindman
+*
+* Return: Always 0 (sucess)
+*/
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
