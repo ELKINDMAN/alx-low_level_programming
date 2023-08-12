5d6c38add3fd7abbc5e66a8ad9e230ec3e5b88f4
ELKINDMAN <elkanahkindness@gmail.com>
2023-08-12 15:50:42 -0700

    task complete

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point for the program
+*
+* Description: program that outputs string using printf function.
+* Author: Elkanah kindman
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
