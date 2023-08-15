1888d0b55aca734f2c0c2d33b60af35765202a86
ELKINDMAN <elkanahkindness@gmail.com>
2023-08-14 19:39:22 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of the program
+ *
+ * description - Outputs numbers in base 10 from 1 to 9
+ *
+ * Author - ELKINDMAN
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int ch;
+
+        for (ch = 0; ch <= 9; ch++)
+        {
+                putchar(ch + '0');
+        }
+
+        putchar('\n');
+
+        return (0);
+}
