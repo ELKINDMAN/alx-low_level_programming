494db8e635981181496016bc7e71a56f7af8d940
ELKINDMAN <elkanahkindness@gmail.com>
2023-08-14 18:08:28 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of the program
+ *
+ * description - Outputs the alphabets except from letter 'e' and 'q'
+ *
+ * Author - ELKINDMAN
+ *
+ * Ruturn: Always 0 (Success)
+ */
+
+int main()
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if (ch != 'e' && ch != 'q')
+		{
+			printf("%c", ch);
+		}
+	printf("\n");
+
+	return (0);
+}
