17be208bc7b00e05b842416100f67492d6c591e5
ELKINDMAN <elkanahkindness@gmail.com>
2023-08-14 17:26:28 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - entry point of the program
+ *
+ * description - outputs the alphabets in uppercase
+ *
+ * Author - ELKINDMAN
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 
 	char ch;
 	
 	for (ch = 'a'; ch <= 'z'; ch++)
-{
+	{
 	putchar(ch);
-}
-for (ch = 'A'; ch <= 'Z'; ch++)
+	}
+for  (ch = 'A'; ch <= 'Z'; ch++)
 {
 	putchar(ch);
 }
