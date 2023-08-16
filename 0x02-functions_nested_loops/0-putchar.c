e54c64981f0a54c17bd4fe680c15704ea97dd6e1
ELKINDMAN <elkanahkindness@gmail.com>
2023-08-16 16:35:52 -0700

    my task complete

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* main - entry point of the program
+*
+* Description: a program that prints a given text
+* Author: Kindness Mbaekwe
+*
+* Return: Always 0 (succes)
+*
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
