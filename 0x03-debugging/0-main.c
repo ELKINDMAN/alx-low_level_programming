984f57d10542cb47f5baa778832b53ce4c9b494f
ELKINDMAN <elkanahkindness@gmail.com>
2023-08-18 20:51:52 -0700

    debugging tasks

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point for the program
+ *
+ * This function tests the positive_or_negative() function.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
