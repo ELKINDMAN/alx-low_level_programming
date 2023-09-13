540b47ec5f8446c5de687847c281a5adc8be3a51
ELKINDMAN <elkanahkindness@gmail.com>
2023-09-12 20:42:56 -0700

    dog.h file created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - sturcture
+ * @name: first member.
+ * @age: second member.
+ * @owner: third member.
+ *
+ * Description: dog details
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char  *owner;
+};
+
+#endif
