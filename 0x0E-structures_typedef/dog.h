a4616a2c9e7bcfad60aa4559e7155061345015c0
ELKINDMAN <elkanahkindness@gmail.com>
2023-09-12 21:30:34 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,11 @@ struct dog
 	char  *owner;
 };
 
+/* function prototypes for structure project */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/*dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);*/
+
 #endif
