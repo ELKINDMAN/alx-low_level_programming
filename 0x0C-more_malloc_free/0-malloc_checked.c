6aeaf701e30a25b6bad978b185872c6c8d66a61f
elkanah <elkanahkindness@gmail.com>
2023-11-02 08:31:26 -0700

    malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that alloctes memory
+ * malloc - function to create space.
+ * @b: parameter for size to allocate.
+ *
+ * Return: pointer to the memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *memAlloc;
+
+	memAlloc = malloc(b);
+	if(memAlloc != NULL)
+	{
+		return (memAlloc);
+	}
+	else
+	{
+		exit (98);
+	}
+}
