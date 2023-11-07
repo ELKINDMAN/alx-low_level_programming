e081d947fd132e873522db0cda5abe9cb3ee9c98
elkanah <elkanahkindness@gmail.com>
2023-11-07 09:06:30 -0800

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Description: a program that prints it's name
+ *
+ * Return: on sucess(0)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
